a38547c5299d4f5797591f9cd3602d44b204de94
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2024-05-23 20:56:55 -0300

    Adding configs

--- products-service/src/main/java/com/estore/products/controller/ProductController.java
+++ products-service/src/main/java/com/estore/products/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.estore.products.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,26 +10,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
-public class ProductsController {
+public class ProductController {
+	
+	private Environment env;
+	private String port;
+	
+	public ProductController(Environment env) {
+		this.env = env;
+		this.port = env.getProperty("local.server.port");
+	}
 
 	@PostMapping
 	public String createProduct() {
-		return "HTTP POST HANDELED";
+		return "HTTP POST HANDELED " + port;
 	}
 	
 	@GetMapping
 	public String getProduct() {
-		return "HTTP GET HANDELED";
+		return "HTTP GET HANDELED " + port;
 	}
 	
 	@PutMapping
 	public String updateProduct() {
-		return "HTTP PUT HANDELED";
+		return "HTTP PUT HANDELED " + port;
 	}
 	
 	@DeleteMapping
 	public String deleteProduct() {
-		return "HTTP DELETE HANDELED";
+		return "HTTP DELETE HANDELED " + port;
 	}
 	
 }
---
+++ products-service/src/main/java/com/estore/products/model/CreateProductRestModel.java
@@ -0,0 +1,17 @@
+package com.estore.products.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class CreateProductRestModel implements Serializable{
+	
+	private static final long serialVersionUID = 5398651485197408970L;
+	
+	private String title;
+	private BigDecimal price;
+	private Integer quantity;
+
+}
