04447cfe27e29c78eca51a9ae101abc4e1913d83
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2024-05-27 11:56:02 -0300

    Add sample test class

--- users-service/src/main/java/com/estore/users/UsersServiceApplication.java
+++ users-service/src/main/java/com/estore/users/UsersServiceApplication.java
@@ -2,8 +2,12 @@ package com.estore.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.estore.core.config.XStreamConfig;
 
 @SpringBootApplication
+@Import({ XStreamConfig.class })
 public class UsersServiceApplication {
 	
 	public static void main(String[] args) {
