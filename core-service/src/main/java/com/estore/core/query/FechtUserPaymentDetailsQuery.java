3ee22197f429d27b189eedad0bdb31caa1401a45
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2024-05-27 11:54:15 -0300

    Add query for payment

---
+++ core-service/src/main/java/com/estore/core/query/FechtUserPaymentDetailsQuery.java
@@ -0,0 +1,14 @@
+package com.estore.core.query;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FechtUserPaymentDetailsQuery {
+	
+	private String userId; 
+
+}
