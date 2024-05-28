a959afaf3153b3c4a75046a48fe3422746468f9a
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2024-05-27 21:10:34 -0300

    Fix confs

--- payments-sevice/src/main/java/com/estore/payments/PaymentsSeviceApplication.java
+++ payments-sevice/src/main/java/com/estore/payments/PaymentsSeviceApplication.java
@@ -2,12 +2,12 @@ package com.estore.payments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 import com.estore.core.config.XStreamConfig;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 @Import({ XStreamConfig.class })
 public class PaymentsSeviceApplication {
